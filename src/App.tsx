ea9375a7f70b47cb5f4c7037e442c0deb4e152aa
erik-frontend <130284367+erik-frontend@users.noreply.github.com>
2025-10-22 16:07:50 +0200

    commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <div className="text-grey-500 bg-grey-50 app text">
+      <h1>My app</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.ts
@@ -0,0 +1,5 @@
+import {Config} from "tailwindcss"
+
+const config: Config = {
+    content: ["./index.html", "./src/**/*.{js, ts, jsx, tsx}"]
+}
