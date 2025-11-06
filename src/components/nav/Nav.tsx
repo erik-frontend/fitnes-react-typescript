8f48aa43320020f8641e6aed3b1954070e6dcced
erik-frontend <130284367+erik-frontend@users.noreply.github.com>
2025-11-06 15:07:01 +0100

    commit

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,9 +1,11 @@
 import { navLinks, type SectionId } from "@/types/type"
 import CustomLink from "./Link"
 import { useState } from "react"
+import logo from "../../assets/Logo.png"
 
 const Nav = () => {
-
+    const [isTopPage, setIsTopPage] = useState<boolean>(true)
+    const navBarBackground = isTopPage ? "" : "bg-primary-100 drop-shadow"
     const [activeSection, setActiveSection] = useState<SectionId|null>(navLinks[0]?.id || null)
 
     const handleActive = (sectionId:SectionId) => {
@@ -12,12 +14,11 @@ const Nav = () => {
 
     return (
         <nav className="relative h-[88px]">
-            <div className="">
-                <div className="">
-                    <div className="">
-                        <img src="" alt="logo" />
-                    </div>
-                    <div className="flex-between w-full">
+            <div className={`${navBarBackground} ${isTopPage ? "py-6" : "py-4"} animate fixed top-0 z-30 w-full`}>
+                <div className="flex-between mx-auto w-5/6">
+                    <div className="flex-between w-full gap-20">
+                        <img src={logo} alt="logo" />
+                        <div className="flex-between w-full">
                         <div className="flex-between gap-8 text-sm">
                             {navLinks.map(({ id, label }) => (
                                 <CustomLink 
@@ -30,6 +31,7 @@ const Nav = () => {
                                 </CustomLink>
                             ))}
                         </div>
+                    </div>
                     </div>
                 </div>
             </div>
